b850ef5c50f97457fa367b25e5830e57b0ff9119
Andreszuluga <carlosandrescespedesz22@gmail.com>
2026-05-04 21:54:39 -0500

    Generate firsts  methods

---
+++ Taller_5/DoubleList/DoubleLinkedList.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DoubleList;
+
+public class DoubleLinkedList<T> where T : IComparable<T>
+{
+    private Node<T>? _head;
+    private Node<T>? _tail;
+
+    public DoubleLinkedList()
+    {
+        _head = null;
+        _tail = null;
+    }
+
+    override public string ToString()
+
+    {
+        var current = _head;
+        var result = string.Empty;
+        while (current != null)
+        {
+            result += $"{current.Data} -> ";
+
+            current = current.Next;
+        }
+        result += "null";
+
+        return result;
+
+    }
+
+    public string ToStringReverce()
+
+    {
+        var current = _tail;
+        var result = string.Empty;
+        while (current != null)
+        {
+            result += $"{current.Data} -> ";
+
+            current = current.Previous;
+        }
+        result += "null";
+
+        return result;
+
+    }
+
+    public void Add(T data)
+    {
+        var newNode = new Node<T>(data);
+
+        if (_head == null)
+        {
+            _head = newNode;
+            return;
+        }
+
+        Node<T> current = _head;
+
+        if (string.Compare(data.ToString(), _head.Data!.ToString()) < 0)
+        {
+            newNode.Next = _head;
+            _head.Previous = newNode;
+            _head = newNode;
+            return;
+        }
+
+        while (current.Next != null && current.Next.Data!.CompareTo(data) < 0)
+        {
+            current = current.Next;
+        }
+
+        newNode.Next = current.Next;
+
+        if (current.Next != null)
+        {
+
+            current.Next.Previous = newNode;
+
+        }
+        else
+        {
+
+            _tail = newNode;
+
+        }
+        current.Next = newNode;
+        newNode.Previous = current;
+    }
+
+    public void SortDescending()
+    {
+        if (_head == null) return;
+
+        var current = _head;
+
+        while (current != null)
+        {
+            var next = current.Next;
+
+            while (next != null)
+            {
+                if (current.Data!.CompareTo(next.Data) < 0)
+                {
+                    var temp = current.Data;
+                    current.Data = next.Data;
+                    next.Data = temp;
+                }
+                next = next.Next;
+            }
+            current = current.Next;
+        }
+    }
+}
+
+     
+        
+     
---
+++ Taller_5/LinkedLists.Console/Program.cs
@@ -0,0 +1,72 @@
+﻿using DoubleList;
+
+var list = new DoubleLinkedList<string>();
+var option = string.Empty;
+var value = string.Empty;
+
+do
+{
+    option = Menu();
+    switch (option)
+    {
+        case "1":
+            Console.Write("Enter value: ");
+            value = Console.ReadLine() ?? string.Empty;
+            list.Add(value);
+            break;
+        case "2":
+            Console.WriteLine(list.ToString());
+            break;
+        case "3":
+            Console.WriteLine(list.ToStringReverse());
+            break;
+        case "4":
+            list.SortDescending();
+            Console.WriteLine("List sorted in descending order.");
+            break;
+        case "5":
+            Console.WriteLine($"The mode(s) are: {list.ShowModes()}");
+            break;
+        case "6":
+            Console.WriteLine(list.ShowGraphic());
+            break;
+        case "7":
+            Console.Write("Enter value: ");
+            value = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine(list.Exists(value) ? "¡Exists!" : "¡Doesn't exist!");
+            break;
+        case "8":
+            Console.Write("Enter value: ");
+            value = Console.ReadLine() ?? string.Empty;
+            list.RemoveOccurrence(value);
+            Console.WriteLine($"Removed one occurrence of {value}.");
+            break;
+        case "9":
+            Console.Write("Enter value: ");
+            value = Console.ReadLine() ?? string.Empty;
+            int removed = list.RemoveAllOccurrences(value);
+            break;
+        case "0":
+            Console.WriteLine("Exiting...");
+            break;
+        default:
+            Console.WriteLine("Invalid option. Try again.");
+            break;
+    }
+} while (option != "0");
+
+string Menu()
+{
+    Console.WriteLine("1. Add. ");
+    Console.WriteLine("2. Show list forward. ");
+    Console.WriteLine("3. Show list backward. ");
+    Console.WriteLine("4. Sort descending. ");
+    Console.WriteLine("5. Show modes. ");
+    Console.WriteLine("6. Show graphic.");
+    Console.WriteLine("7. Exists. ");
+    Console.WriteLine("8. Remove an occurrence. ");
+    Console.WriteLine("9. Remove all occurrences. ");
+    Console.WriteLine("0. Exit. ");
+    Console.Write("Enter your option: ");
+    return Console.ReadLine() ?? string.Empty;
+}
